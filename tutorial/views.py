1b0fcda527a74d6ef53a603577037ee7e6189d58
saimkhan92 <saim.khan@colorado.edu>
2018-03-27 17:49:31 -0700

    extracted access token from authorization code

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from tutorial.authhelper import get_signin_url
+from tutorial.outlookservice import get_me
+from tutorial.authhelper import get_token_from_code
+
+
+# Create your views here.
 
 def home(request):
-  sign_in_url = '#'
-  context = { 'signin_url': sign_in_url }
-  return render(request, 'tutorial/home.html', context)
-# Create your views here.
+  redirect_uri = request.build_absolute_uri(reverse('tutorial:gettoken'))
+  sign_in_url = get_signin_url(redirect_uri)
+  return HttpResponse('<a href="' + sign_in_url +'">Click here to sign in and view your mail</a>')
+
+def gettoken(request):
+  auth_code = request.GET['code']
+  redirect_uri = request.build_absolute_uri(reverse('tutorial:gettoken'))
+  token = get_token_from_code(auth_code, redirect_uri)
+  access_token = token['access_token']
+  user = get_me(access_token)
+
+  # Save the token in the session
+  request.session['access_token'] = access_token
+  return HttpResponse('User: {0}, Access token: {1}'.format(user['displayName'], access_token))
